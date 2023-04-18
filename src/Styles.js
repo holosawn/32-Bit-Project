2911308c4bbf9c3d6e528795e0448aef1d75aafd
holosawn <veilder9@gmail.com>
2023-04-19 01:53:56 +0300

    message

---
+++ src/Styles.js
@@ -0,0 +1,14 @@
+const labelStyle={
+    fontWeight:700,
+    whiteSpace:"normal",
+    margin:1,
+    minWidth:"110px"
+}
+const formBoxStyle={
+    width:"auto",
+    display:"flex",
+    justifyContent:"space-between",
+    alignItems:"center"
+}
+
+export {labelStyle,formBoxStyle}
