40a039c0f6ff4c29c8ff4826f9a9cce51e3c1580
holosawn <veilder9@gmail.com>
2023-06-13 16:16:52 +0300

    changed the virtualTable as a separate component

--- src/HataListe/PrepareData.js
+++ src/HataListe/PrepareData.js
@@ -3,12 +3,21 @@ const prepareData = (data) => {
     const {defectList , nrReasonList} = data
     let id = 0
     const rows = defectList.map(obj => {
-        const {formattedAssyNo , depCode , formattedBodyNo , vinNo , colorExtCode , rgbCode , modelCode , termId , partName , spotCode , spotgunName , arcnutboltgunName , arcnutboltCode , defectName , defrankCode , formattedDefectHour , defectType , defrespName , defectReason} = obj
+        const {formattedAssyNo , depCode , depId , formattedBodyNo , vinNo , colorExtCode , rgbCode , modelCode , termId , partName , spotCode , spotgunName , arcnutboltgunName , arcnutboltCode , defectName , defrankCode , formattedDefectHour , defectType , defrespName , defectReason} = obj
         const colorData = {colorExtCode , rgbCode}
         id ++
 
-        return {id ,formattedAssyNo , depCode , formattedBodyNo , vinNo , colorData , modelCode , termId , partName , spotCode , spotgunName , arcnutboltgunName , arcnutboltCode , defectName , defrankCode , formattedDefectHour , defectType , defrespName , defectReason }
+        return {id ,formattedAssyNo , depCode , depId , formattedBodyNo , vinNo , colorData , modelCode , termId , partName , spotCode , spotgunName , arcnutboltgunName , arcnutboltCode , defectName , defrankCode , formattedDefectHour , defectType , defrespName , defectReason }
     })
+
+    rows.sort((a) => {
+        if(a.depId === 94){
+            return -1
+        }else{
+            return 1
+        }
+    })
+    
 
     return {rows,nrReasonList}
 }
