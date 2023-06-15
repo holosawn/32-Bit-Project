d46eec39f15e90695421d1d4b7d208fe8a0589f5
holosawn <veilder9@gmail.com>
2023-06-15 14:31:53 +0300

    Revert "acı"
    This reverts commit 73adb5a4801e19ca02c6bbc5ca33094b54374d36.

--- src/TerminalLogin/Login.js
+++ src/TerminalLogin/Login.js
@@ -1,64 +1,37 @@
+
 import {AppBar,Toolbar,Typography,Button } from '@mui/material';
 import './Login.css';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import Data from '../GetData';
+import Box from '@mui/material/Box';
 import { Formik, Form, ErrorMessage, Field ,useFormik} from "formik";
 import * as yup from "yup";
-import SelectWrapper from "./CustomSelect"
-import TextfieldWrapper from './Custominput';
 
-const opptionss={
-	"0":"developer",
-	"1":"berber"
-}
 
-const initialFormState={
-	terminal:"",
-	sicil:"",
-	password:"",
-	montaj:""
-}
-const formValidation= yup.object().shape({
-	terminal: yup.string()
-	.oneOf(["developer","manav","berber"])
-	.required("required"),
-	sicil: yup.string()
-	.required("required"),
-	password: yup.string()
-	.required("required"),
-	montaj: yup.string()
-	.required("required")
-})
 
-export default function Login(){ 
-	return(
-	<Formik
-       initialValues={{initialFormState}}
-	   validationSchema={formValidation}
+
+export default function Login(){ return(<Formik
+       initialValues={{ name: 'jared' }}
        onSubmit={(values, actions) => {
-         console.log("submitted")
+         setTimeout(() => {
+           alert(JSON.stringify(values, null, 2));
+           actions.setSubmitting(false);
+         }, 1000);
        }}
      >
-		{props => (<Form>
-       
-		<SelectWrapper
-            name="terminal"
-            label="Terminal"
-            options={opptionss}
-        />
-
-		<TextfieldWrapper 
-		name="sicil"
-		label="Sicil No"
-		></TextfieldWrapper>
-	   
-	   <TextfieldWrapper
-	   name="password"
-	   label="Şifre"
-	   ></TextfieldWrapper>
-	   
-           <button type='submit'>Submit</button>
-         </Form>)}
-       
+       {props => (
+         <Form onSubmit={props.handleSubmit}>
+           <input
+             type="text"
+             onChange={props.handleChange}
+             onBlur={props.handleBlur}
+             value={props.values.name}
+             name="name"
+           />
+          
+           <button type="submit">Submit</button>
+         </Form>
+       )}
      </Formik>
 )}
